320dfa46292cbd81c9f385697ff0d472c22b2a82
Liburkus <150595918+Liburkus@users.noreply.github.com>
2023-11-13 16:54:15 -0300

    Update button background index.js

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-function shareOnSocialMedia() {
-    alert('Compartilhando nas redes sociais!'); 
-}
-
+        document.addEventListener("DOMContentLoaded", function () {
+            var botao = document.createElement("button");
+            botao.textContent = "Trocar Plano de Fundo";
+            document.body.appendChild(botao);
+        
+            var imagens = [
+                "url('img/4K_60FPS_Omen_Animated_Wallpaper_Valorant_Fanart.gif')",
+                "url('img/bg0.jpg')"
+            ];
+        
+            var indiceAtual = 0;
+        
+            botao.addEventListener("click", function () {
+               
+                document.body.style.backgroundImage = imagens[indiceAtual];
+                document.body.style.backgroundSize = "cover";
+                document.body.style.backgroundRepeat = "no-repeat";
+        
+                indiceAtual = (indiceAtual + 1) % imagens.length;
+            });
+        });
+        
